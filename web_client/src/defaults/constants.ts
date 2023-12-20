4df2eceb22030569591123823a561300a661908a
chrishuangcf <51814709+chrishuangcf@users.noreply.github.com>
2023-12-20 00:27:42 -0800

    Update constants.ts

--- web_client/src/defaults/constants.ts
+++ web_client/src/defaults/constants.ts
@@ -1,9 +1,1 @@
-export const
-  DEFAULT_PORT = 8088,
-  PROXY_PORT = 7503,
-  DEVICE_ID = 'urn:uuid:ce8ff292-b7f5-4ff7-a311-445d646d6589',
-  DAAS_ACCESS_TOKEN = 'af6bf05e-c50e-4724-b1a2-04b0d99da859',
-  DAAS_ENDPOINT = '',
-  DAAS_ENDPOINT_DEV = 'https://api-devstg.t-mobile.com/v1/dev_digitsApi/',
-  DAAS_ENDPOINT_STAGING = 'https://api-teststg.t-mobile.com/v1/stg_digitsApi/',
-  PROXY_URL = 'http://localhost:8088/proxybox/'
+
